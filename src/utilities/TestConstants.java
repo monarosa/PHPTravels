251a9b59de6915550d436beeb5b6eb947ed3e5ea
monarosa <rukal.61@gmail.com>
2019-02-17 04:32:40 -0500

    new constant is added

--- src/utilities/TestConstants.java
+++ src/utilities/TestConstants.java
@@ -7,5 +7,6 @@ public class TestConstants {
 		public static final String USER_NAME="Tester";
 		public static final String PASSWORD="test";
 		public static final String WEB_ORDER_URL="http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx";
+		public static final String  NEWS="new is added";
 	
 }
